9e5faa75a85b3f5f93c7c376c7608f4de67e32f2
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-02-23 12:10:50 +0000

    ee

--- Assets/Editor/CustomWindow.cs
+++ Assets/Editor/CustomWindow.cs
@@ -1,20 +1,21 @@
-using UnityEditor;
 using UnityEngine;
-
-
+using UnityEditor;
 
 public class CustomWindow : EditorWindow
 {
 
-    [MenuItem("Window/Rooms Randomly")]
-    public static void ShowWindow() //open window
-    {
-        EditorWindow.GetWindow<EditorWindow>("Rooms Randomly");
-    }
+	
 
-    void onGUI()
-    {
-        //Window code put all code for it here
-        GUILayout.Label("This is a label.", EditorStyles.boldLabel);
-    }
+	[MenuItem("Window/Rooms Randomly")]
+	public static void ShowWindow()
+	{
+		GetWindow<CustomWindow>("Rooms Randomly");
+	}
+
+	void OnGUI()
+	{
+
+
+
+	}
 }
