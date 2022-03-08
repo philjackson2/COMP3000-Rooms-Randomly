3941d204d50b1e191f92411acc3a4028bdf5538d
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-03-08 17:25:37 +0000

    got the room instatiate working : )

--- Assets/Editor/CustomWindow.cs
+++ Assets/Editor/CustomWindow.cs
@@ -5,6 +5,7 @@ public class CustomWindow : EditorWindow
 {
 
 	string myString = "Hello World";
+	
 
 	[MenuItem("Window/Rooms Randomly")]
 	public static void ShowWindow()
@@ -19,10 +20,14 @@ public class CustomWindow : EditorWindow
 
 		myString = EditorGUILayout.TextField("Name", myString);//this the stuff for testing a text field. probably not relevant
 
-		if (GUILayout.Button("Press me"))
+		if (GUILayout.Button("Instantiate Base Room")) //if statement for button press will call on the specific object located in the recourcess
         {
 			Debug.Log("Press Sucess");
-        }
 
+			var EntryRoom = Resources.Load<GameObject>("Entry Room"); //takes the specific game object with that name and instatiates it on button press
+
+			Instantiate(EntryRoom);
+		}
+		
 	}
 }
