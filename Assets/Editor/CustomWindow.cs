737476e1769d5a026cbde336b02c05e322f5619f
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-03-08 18:12:07 +0000

    code cleanup

--- Assets/Editor/CustomWindow.cs
+++ Assets/Editor/CustomWindow.cs
@@ -4,7 +4,6 @@ using UnityEditor;
 public class CustomWindow : EditorWindow
 {
 
-	string myString = "Hello World";
 	
 
 	[MenuItem("Window/Rooms Randomly")]
@@ -16,17 +15,17 @@ public class CustomWindow : EditorWindow
 	void OnGUI()
 	{
 
-		GUILayout.Label("Label Test", EditorStyles.boldLabel);
+		GUILayout.Label("Rooms Randomly", EditorStyles.boldLabel);
 
-		myString = EditorGUILayout.TextField("Name", myString);//this the stuff for testing a text field. probably not relevant
+		
 
 		if (GUILayout.Button("Instantiate Base Room")) //if statement for button press will call on the specific object located in the recourcess
         {
-			Debug.Log("Press Sucess");
+			
 
 			var EntryRoom = Resources.Load<GameObject>("Entry Room"); //takes the specific game object with that name and instatiates it on button press
 
-			Instantiate(EntryRoom);
+			Instantiate(EntryRoom); 
 		}
 		
 	}
