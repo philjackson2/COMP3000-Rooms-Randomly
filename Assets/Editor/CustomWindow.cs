1ed2a0201f390c474bf031a43e32e08124a574a7
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-03-19 19:06:38 +0000

    okay thank god i fixed it

--- Assets/Editor/CustomWindow.cs
+++ Assets/Editor/CustomWindow.cs
@@ -4,7 +4,7 @@ using UnityEditor;
 public class CustomWindow : EditorWindow
 {
 
-	
+
 
 	[MenuItem("Window/Rooms Randomly")]
 	public static void ShowWindow()
@@ -17,16 +17,18 @@ public class CustomWindow : EditorWindow
 
 		GUILayout.Label("Rooms Randomly", EditorStyles.boldLabel);
 
-		
+
 
 		if (GUILayout.Button("Instantiate Base Room")) //if statement for button press will call on the specific object located in the recourcess
-        {
-			
+		{
+
 
 			var EntryRoom = Resources.Load<GameObject>("Entry Room"); //takes the specific game object with that name and instatiates it on button press
+			var RoomTemplate = Resources.Load<GameObject>("Room Templates");
 
-			Instantiate(EntryRoom); 
+			Instantiate(EntryRoom);
+			Instantiate(RoomTemplate);
 		}
-		
+
 	}
 }
