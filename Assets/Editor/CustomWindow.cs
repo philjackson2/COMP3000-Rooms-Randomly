da74e74869f1bf8fa188b342944b27b4ccf7bffc
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-03-20 17:03:53 +0000

    new custom window to test stuff. Not permanament

--- Assets/Editor/CustomWindow.cs
+++ Assets/Editor/CustomWindow.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UIElements;
 
 public class CustomWindow : EditorWindow
 {
@@ -12,11 +15,23 @@ public class CustomWindow : EditorWindow
 		GetWindow<CustomWindow>("Rooms Randomly");
 	}
 
+	
 	void OnGUI()
 	{
 
 		GUILayout.Label("Rooms Randomly", EditorStyles.boldLabel);
 
+
+		var splitView = new TwoPaneSplitView(0, 250, TwoPaneSplitViewOrientation.Horizontal);
+
+		// Add the view to the visual tree by adding it as a child to the root element
+		rootVisualElement.Add(splitView);
+
+		// A TwoPaneSplitView always needs exactly two child elements
+		var leftPane = new VisualElement();
+		splitView.Add(leftPane);
+		var rightPane = new VisualElement();
+		splitView.Add(rightPane);
 
 
 		if (GUILayout.Button("Instantiate Base Room")) //if statement for button press will call on the specific object located in the recourcess
