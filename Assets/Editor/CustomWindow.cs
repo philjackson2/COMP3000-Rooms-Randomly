4be6d14495970a2546cc2d9c517b034ea2a4daf5
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-03-22 15:42:30 +0000

    e

--- Assets/Editor/CustomWindow.cs
+++ Assets/Editor/CustomWindow.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.UIElements;
+using System.IO;
 
 public class CustomWindow : EditorWindow
 {
@@ -20,23 +21,8 @@ public class CustomWindow : EditorWindow
 	{
 
 		GUILayout.Label("Rooms Randomly", EditorStyles.boldLabel);
-
 		
-
-		var splitView = new TwoPaneSplitView(0, 250, TwoPaneSplitViewOrientation.Horizontal);
-
-		// Add the view to the visual tree by adding it as a child to the root element
-		rootVisualElement.Add(splitView);
-
-		// A TwoPaneSplitView always needs exactly two child elements
-		var leftPane = new VisualElement();
-		splitView.Add(leftPane);
-		var rightPane = new VisualElement();
-		splitView.Add(rightPane);
-
-
-		
-
+									
 		if (GUILayout.Button("Instantiate Base Room")) //if statement for button press will call on the specific object located in the recourcess
 		{
 
