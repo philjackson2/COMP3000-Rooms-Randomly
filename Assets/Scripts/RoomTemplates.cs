f8c19bff3e6d4ee6d73baa10f079dee9da5914f5
philjackson2 <91661667+philjackson2@users.noreply.github.com>
2022-04-20 21:07:29 +0100

    CHEST WORKING INGIDNIGNDSIGNDING

--- Assets/Scripts/RoomTemplates.cs
+++ Assets/Scripts/RoomTemplates.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class RoomTemplates : MonoBehaviour {
-	
+
 
 	public GameObject[] bottomRooms;
 	public GameObject[] topRooms;
@@ -16,7 +18,21 @@ public class RoomTemplates : MonoBehaviour {
 
 	public float waitTime;
 	private bool spawnedBoss;
+	private bool spawnedChest;
 	public GameObject boss;
+	public int chestRoomSpawn; //declaring public int for user to delare which room they want the chest in
+	public GameObject chest;
+
+
+	
+
+
+
+
+
+
+
+
 
 	void Update(){
 
@@ -25,10 +41,31 @@ public class RoomTemplates : MonoBehaviour {
 				if(i == rooms.Count-1){
 					Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
 					spawnedBoss = true;
+					
 				}
 			}
 		} else {
 			waitTime -= Time.deltaTime;
 		}
+
+		if (waitTime <= 0 && spawnedChest == false)
+		{
+		 for (int i = 0; i < rooms.Count; i++)
+			{
+				if (i == rooms.Count - 1)
+				{
+					Instantiate(chest, rooms[chestRoomSpawn].transform.position, Quaternion.identity); //puts the chest into the user determined room based on the code used for boss
+					spawnedChest = true;
+
+				}
+			}
+		}
+		else
+		{
+			waitTime -= Time.deltaTime;
+		}
+
+
+
 	}
 }
